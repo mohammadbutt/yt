7ecf8d0f57620eab0b1b0f72a9c483ac3c6854d8
Mohammad Butt <maniqrb@gmail.com>
2019-12-30 13:48:23 -0800

    Added vim_commands file

---
+++ C_Programming_Language/03_vim_commands/vim_commands.c
@@ -0,0 +1,22 @@
+1. I - Insert Mode
+2. Esc - Escape Mode
+3. Esc + :w - Save File
+4. Esc + :q - Close File
+4. Esc + :wq - Save and Close File
+5. Esc + :syntax on - Enable Syntax highlight
+6. Esc + :set mouse=a - Enables Mouse
+7. Esc + y - Yank / Copies
+8. Esc + p - Paste
+9. Esc + x - Cut
+10. Esc + d - Delete
+11. Esc + u - Undo
+12. Esc + :set number - Enables Lines numbers
+13. Esc + /word - Finds word or phrase
+14. Esc + :set hlsearch - Enable search highlight
+15. Esc + :noh - Disable search highlight
+16. Esc + :24 - Go to Line
+17. Esc + :set colorcolumn=24 - Set Margin
+18. Ctrl + n - Word Completion
+19. Esc + Ctrl + v - Multiple line selection
+20. Esc + :%s/vim/destination/gc - Search and replace
+20. Esc + :25,30s/destination/vim/gc - Search and replace within line range
