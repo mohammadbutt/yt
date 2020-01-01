0f7b6e4d3592430f229f2354a4d7d6a0a5a4cbfc
Mohammad Butt <maniqrb@gmail.com>
2019-12-31 22:17:43 -0800

    Update vim_commands.c

--- C_Programming_Language/03_vim_commands/vim_commands.c
+++ C_Programming_Language/03_vim_commands/vim_commands.c
@@ -1,27 +1,27 @@
-1. I - Insert Mode
-2. Esc - Escape Mode
-3. Esc + :w - Save File
-4. Esc + :q - Close File
-4. Esc + :wq - Save and Close File
-5. Esc + :syntax on - Enable Syntax highlight
-6. Esc + :set mouse=a - Enables Mouse
-7. Esc + y - Yank / Copies
-8. Esc + p - Paste
-9. Esc + x - Cut
-10. Esc + d - Delete
-11. Esc + u - Undo
-12. Esc + :set number - Enables Lines numbers
-13. Esc + /word - Finds word or phrase
-14. Esc + :set hlsearch - Enable search highlight
-15. Esc + :noh - Disable search highlight
-16. Esc + :24 - Go to Line
-17. Esc + :set colorcolumn=24 - Set Margin
-18. Ctrl + n - Word Completion
-19. Esc + Ctrl + v - Multiple line selection
-20. Esc + :%s/vim/destination/gc - Search and replace
+1. I --------------------------------- Insert Mode
+2. Esc ------------------------------- Escape Mode
+3. Esc + :w -------------------------- Save File
+4. Esc + :q -------------------------- Close File
+4. Esc + :wq ------------------------- Save and Close File
+5. Esc + :syntax on ------------------ Enable Syntax highlight
+6. Esc + :set mouse=a ---------------- Enables Mouse
+7. Esc + y --------------------------- Yank / Copies
+8. Esc + p --------------------------- Paste
+9. Esc + x --------------------------- Cut
+10. Esc + d -------------------------- Delete
+11. Esc + u -------------------------- Undo
+12. Esc + :set number ---------------- Enables Lines numbers
+13. Esc + /word ---------------------- Finds word or phrase
+14. Esc + :set hlsearch -------------- Enable search highlight
+15. Esc + :noh ----------------------- Disable search highlight
+16. Esc + :24 ------------------------ Go to Line
+17. Esc + :set colorcolumn=24 -------- Set Margin
+18. Ctrl + n ------------------------- Word Completion
+19. Esc + Ctrl + v ------------------- Multiple line selection
+20. Esc + :%s/vim/destination/gc ----- Search and replace
 20. Esc + :25,30s/destination/vim/gc - Search and replace within line range.
-21. Esc + :set autoindent - Autoindent after you initialize variables or type, if and while statements.
-22. Esc + :set cindent - Auto indents after curly brackets.
-23. Esc + shiftwidth=4 - Sets indentation width at 4 character space instead of 8.
-24. Esc + setsofttabstop=4 - If shiftwidth is turned on then backspace removes 1 space instead of tab. setsofttabstop will
-                             remove the full tab with one backspace.
+21. Esc + :set autoindent ------------ Autoindent after you initialize variables or type, if and while statements.
+22. Esc + :set cindent --------------- Auto indents after curly brackets.
+23. Esc + shiftwidth=4 --------------- Sets indentation width at 4 character space instead of 8.
+24. Esc + setsofttabstop=4 ----------- If shiftwidth is turned on then backspace removes 1 space instead of tab. setsofttabstop
+                                       will remove the full tab with one backspace.
