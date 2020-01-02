479542a18b536b8b497372d988449504fc1aadb8
Mohammad Butt <maniqrb@gmail.com>
2020-01-01 19:38:30 -0800

    Update vim_commands.c

--- C_Programming_Language/03_vim_commands/vim_commands.c
+++ C_Programming_Language/03_vim_commands/vim_commands.c
@@ -22,6 +22,6 @@
 20. Esc + :25,30s/destination/vim/gc - Search and replace within line range.
 21. Esc + :set autoindent ------------ Autoindent after you initialize variables or type, if and while statements.
 22. Esc + :set cindent --------------- Auto indents after curly brackets.
-23. Esc + shiftwidth=4 --------------- Sets indentation width at 4 character space instead of 8.
-24. Esc + setsofttabstop=4 ----------- If shiftwidth is turned on then backspace removes 1 space instead of tab.
+23. Esc + :set shiftwidth=4 --------------- Sets indentation width at 4 character space instead of 8.
+24. Esc + :set softtabstop=4 ----------- If shiftwidth is turned on then backspace removes 1 space instead of tab.
                                        setsofttabstop will remove the full tab with one backspace.
